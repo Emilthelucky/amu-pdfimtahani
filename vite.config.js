4e80c01b5194abdad1cb8ebf8624577f9dd6ef27
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-07-01 18:32:14 +0400

    update

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'https://amu-exam-api-v2.onrender.com',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
             },
