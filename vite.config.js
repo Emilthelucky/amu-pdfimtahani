9b67c0b425d2f7f9e47007575e4d4707361d75ae
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-07-01 17:48:13 +0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://amu-exam-api-v2.onrender.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
-})
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         proxy: {
+//             '/api': {
+//                 target: 'https://amu-exam-api-v2.onrender.com',
+//                 changeOrigin: true,
+//                 rewrite: (path) => path.replace(/^\/api/, ''),
+//             },
+//         },
+//     },
+// })
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
@@ -29,3 +29,14 @@ export default defineConfig({
 //         },
 //     },
 // })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000, // Vercel veya Render'in ayarladığı PORT değişkenini kullanabilirsiniz.
+    },
+})
