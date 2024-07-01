aa4ec3bb70ed27eb276ca33ce1998bd99ae837a3
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-07-01 17:55:35 +0400

    update

--- vite.config.js
+++ vite.config.js
@@ -37,6 +37,13 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 3000, // Vercel veya Render'in ayarladığı PORT değişkenini kullanabilirsiniz.
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
     },
 })
