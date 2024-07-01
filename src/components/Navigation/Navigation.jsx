595ccda4382a0d786e503e8d7055773e1f49250c
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-07-01 19:11:33 +0400

    production

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,6 +8,12 @@ export const Navigation = () => {
                 <Link to="/" className="image-container">
                     Imtahan ver
                 </Link>
+                <p className="qeyd">
+                    Qeyd: Saytda olsun ux tərəfdə olsun funksionallıq tərəfdə
+                    çox əksiklik var sayt ümümi 10 saata qurulub. Bunu nəzərə
+                    alın. digər semestr bütün funksialar sizə təmin olunmuş
+                    şəkildə qarşınızda olacaq.
+                </p>
                 <ul className="navigations">
                     <li>
                         <Link to="/">Ev</Link>
